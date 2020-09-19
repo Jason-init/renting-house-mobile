ad4a1fc5cdd6a2601f4549e6072c70f1f0519ca9
QiZhang <jason.qizhang.b@gmail.com>
2020-09-19 20:56:23 +0800

    Added TabBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // import { Button } from 'antd-mobile'
 
@@ -11,15 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Button>按钮</Button> */}
-        <ul>
-          <li>
-            <Link to="/home">home主页</Link>
-          </li>
-          <li>
-            <Link to="/citylist">城市列表页面</Link>
-          </li>
-        </ul>
+       
         <Route path="/home" component={Home} />
         <Route path="/citylist" component={CityList} />
       </div>
